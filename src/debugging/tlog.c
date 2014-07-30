d6dc529be3289f2679824a2dd10adf8e7f71f9a2
Gauthier Picalausa <mc2m@uplinklabs.net>
2014-07-30 17:05:02 +0200

    Allow no logging at all

--- src/debugging/tlog.c
+++ src/debugging/tlog.c
@@ -10,41 +10,43 @@ const char *cattotext[] = {"","Warning: ","Error: "};
 void TLogSetFile(FILE *_file)
 {
 	tLogFile = _file;
-	if(!tLogFile) {
-		tLogFile = stdout;
-		TLogReport(T_LOG_WARNING,"TLogSetFile","The file pointer used for logging is null. stdout will be used instead.");
-	}
 }
 
 void TLogReport(int category, const char *function, const char *format,...)
 {
-	va_list ap;
+	if(tLogFile) {
+		va_list ap;
 
-	TLogStartReport(category,function);
+		TLogStartReport(category,function);
 
-	va_start(ap, format);
-	vfprintf(tLogFile,format,ap);
-	va_end(ap);
+		va_start(ap, format);
+		vfprintf(tLogFile,format,ap);
+		va_end(ap);
 
-	fprintf(tLogFile,"\n");
+		fprintf(tLogFile,"\n");
+	}
 }
 
 void TLogStartReport(int category, const char *function)
 {
-	fputs(cattotext[category],tLogFile);
-	if(category > 0) fprintf(tLogFile,"In function %s: ", function);
+	if(tLogFile) {
+		fputs(cattotext[category],tLogFile);
+		if(category > 0) fprintf(tLogFile,"In function %s: ", function);
+	}
 }
 
 void TLogWrite(const char *format,...)
 {
-	va_list ap;
+	if(tLogFile) {
+		va_list ap;
 
-	va_start(ap, format);
-	vfprintf(tLogFile,format,ap);
-	va_end(ap);
+		va_start(ap, format);
+		vfprintf(tLogFile,format,ap);
+		va_end(ap);
+	}
 }
 
 void TLogWriteV(const char *format,va_list ap)
 {
-	vfprintf(tLogFile,format,ap);
+	if(tLogFile) vfprintf(tLogFile,format,ap);
 }
